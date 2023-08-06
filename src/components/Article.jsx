edf24b9831b9cc32d35e5dbd7985ff2d756c5e4c
= <=>
2023-08-06 11:01:48 -0700

    update1

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Article({ title, date = "January 1, 1970", preview, minutes }) {
-  
   const minuteDisplay = minutes < 30
     ? "☕️".repeat(Math.ceil(minutes / 5)) + ` ${minutes} min read`
     : "🍱".repeat(Math.ceil(minutes / 10)) + ` ${minutes} min read`;
